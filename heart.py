ed8be814d41c3246cd7176f357decd98635fbdc4
binaryman008 <ankush25kumar@gmail.com>
2019-03-20 23:48:39 +0530

    added feature scalings

--- heart.py
+++ heart.py
@@ -6,16 +6,25 @@ import numpy as np
 
 #importing data
 dataset = pd.read_csv("heart.csv")
-X = dataset.drop('target',axis = 1)
+X = dataset.drop('target',axis=1)
 y = dataset.iloc[:,-1].values
 
-#try feature scaling here if possible
 
 # Splitting the dataset into the Training set and Test set
 
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=109)
+
+
+# Featrue scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+
 
 # Fitting Logistic Regression to the Training set
 
